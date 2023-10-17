2d5e855842151bc51b7d25ca814c4f14fd3f2d24
Ashis <developer.ashis99@gmail.com>
2023-10-18 03:40:42 +0530

    Added structure for habits

--- client/src/components/Home/index.jsx
+++ client/src/components/Home/index.jsx
@@ -1,20 +1,50 @@
-import FooterNav from '../FooterNav';
-import HeaderNav from '../HeaderNav';
-import './home.styles.css'
+import FooterNav from "../FooterNav";
+import HeaderNav from "../HeaderNav";
+import { Checkbox } from 'antd';
+import "./home.styles.css";
 
-const Home = ()=>{
-    return(
+const Home = () => {
+    return (
         <div className="home-container">
             <div className="home-body">
-            <HeaderNav/>
+                {/* Header Part */}
+                <HeaderNav />
 
-            <div className="home-content">
+                {/* Home Content */}
+                <div className="home-content">
+                    <div className="wish-user">
+                        <h2>Good Morning, Ashis!</h2>
+                        <span className="todays-date">October 18th, 2023</span>
+                    </div>
 
+                    <div className="todays-stats-container">
+                        <h4>Today's Stats</h4>
+                        <div className="show-mini-stat"></div>
+                    </div>
+
+                    <div className="habits-list-container">
+                        <h4>Today's Habits</h4>
+
+                        <div className="habit-list">
+                            {/* Individual habits */}
+                            <div className="habit-list-items">
+                                <div className="habit-data">
+                                    <img className="habit-img" src="" alt="" />
+                                    <div className="habit-detail">
+                                        <h5>Habit Name</h5>
+                                        <span>08:00AM</span>
+                                    </div>
+                                </div>
+                                <Checkbox  className="habit-status"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            </div>
-            <FooterNav/>
+            {/* Footer Part */}
+            <FooterNav />
         </div>
-    )
-}
+    );
+};
 
 export default Home;
